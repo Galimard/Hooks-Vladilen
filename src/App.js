40eca6316bb4a37dd9bff85a52143d4ecf92a306
Galimard <galimard.maria@gmail.com>
2022-03-11 13:00:12 +0800

    useCallback

--- src/App.js
+++ src/App.js
@@ -1,13 +1,19 @@
-import React, {useState, useEffect, useRef, useMemo} from "react";
+import React, {useState, useCallback} from "react";
 
 function App() {
+  const [colored, setColored] = useState(false);
+  const [count, setCount] = useState(1);
+
+  const styles = {
+    color: colored ? 'darkred' : 'black'
+  }; 
 
   return (
     <div>
-      <h1>Вычисляемое свойство: </h1>
-      <button className="btn btn-success" onClick={() => setNumber(prev => prev + 1)}>Добавить</button>
-      <button className="btn btn-danger" onClick={() => setNumber(prev => prev - 1)}>Убрать</button>
+      <h1 style={styles}>Количество элементов: {count}</h1>
+      <button className="btn btn-success" onClick={() => setCount(prev => prev + 1)}>Добавить</button>
       <button className="btn btn-warning" onClick={() => setColored(prev => !prev)}>Изменить</button>
+
     </div>
   );
 }
