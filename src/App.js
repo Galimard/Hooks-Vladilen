f677ea6d9561fe0e9d7370752c314548cda91272
Galimard <galimard.maria@gmail.com>
2022-03-11 12:46:10 +0800

    useMemo

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
-import React, {useState, useEffect, useRef} from "react";
+import React, {useState, useEffect, useRef, useMemo} from "react";
 
 function App() {
 
   return (
     <div>
-      <h1>Количество рендеров: </h1>
+      <h1>Вычисляемое свойство: </h1>
+      <button className="btn btn-success" onClick={() => setNumber(prev => prev + 1)}>Добавить</button>
+      <button className="btn btn-danger" onClick={() => setNumber(prev => prev - 1)}>Убрать</button>
+      <button className="btn btn-warning" onClick={() => setColored(prev => !prev)}>Изменить</button>
     </div>
   );
 }
