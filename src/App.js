2dd89cf6f2d2323df5e333311e1cd837ac785cb9
Galimard <galimard.maria@gmail.com>
2022-03-11 14:05:56 +0800

    custom hook

--- src/App.js
+++ src/App.js
@@ -1,16 +1,44 @@
-import React, {useContext} from "react";
-import Alert from "./alert/Alert";
-import { AlertProvider } from "./alert/AlertContext";
-import Main from "./Main";
+import React, {useState, useEffect} from "react";
+
+function useLogger(value) {
+  useEffect(() => {
+    console.log(value);
+  }, [value]);
+}
+
+function useInput(initialValue) {
+  const [value, setValue] = useState(initialValue);
+
+  const onChange = event => {
+    setValue(event.target.value);
+  }
+
+  const clear = () => setValue('');
+
+  return {
+    bind: {value, onChange},
+    value,
+    clear
+  }
+}
 
 function App() {
+  const input = useInput('');
+  const lastName = useInput('');
+
+  useLogger(input.value);
+
   return (
-    <AlertProvider>
-      <div className="container pt-3">
-        <Alert />
-        <Main toggle={() => {}} />      
-      </div>
-    </AlertProvider>    
+      <div className="container pt-3">   
+      {/* одинаковые названия ключей => spread {...input} */}
+        <input type="text" {...input.bind} /> 
+        <input type="text" {...lastName} /> 
+        {/* <input type="text" value={input.value} onChange={input.onChange} />     */}
+        <button className="btn btn-warning" onClick={() => input.clear()}>Очистить</button>
+        <hr />   
+        
+        <h1>{input.value}</h1>        
+      </div>  
   );
 }
 
